ff193a981421f5c43d647c0a0aa648e80ac6779d
no-la <nzyc0216@icloud.com>
2025-06-27 21:18:23 +0900

    refactoring

--- src/handler.ts
+++ src/handler.ts
@@ -2,6 +2,30 @@ import { Notice, Plugin } from "obsidian";
 
 export default class Handler {
 	constructor(private readonly plugin: Plugin) {}
+
+	clickMonthHandler(e: MouseEvent): void {
+		if (!(e.target instanceof HTMLElement)) {
+			console.error("Event target is not an HTMLElement.");
+			return;
+		}
+
+		new Notice(`Month clicked! ${e.target.textContent}`);
+		const COMMAND_ID = "periodic-notes:open-monthly-note";
+		const commands = (this.plugin.app as any).commands;
+		const exists = commands
+			.listCommands()
+			.some((cmd: any) => cmd.id === COMMAND_ID);
+
+		if (!exists) {
+			new Notice(
+				`Command with ID "${COMMAND_ID}" does not exist.\nPlease check if the "Periodic Notes" plugin is enabled.`
+			);
+			return;
+		}
+
+		commands.executeCommandById(COMMAND_ID);
+	}
+
 	addMonthClickListener() {
 		const monthEl = document.querySelector<HTMLElement>(
 			"#calendar-container > div > h3 > span.month.svelte-1vwr9dd"
@@ -10,24 +34,16 @@ export default class Handler {
 		// 	"#calendar-container > div > h3 > span.month.svelte-1vwr9dd"
 		// );
 		if (!monthEl) {
-			console.error("Month element not found.");
+			console.error(
+				'Month element not found.\nPlease check "Calendar" plugin is enabled.'
+			);
 			return;
 		}
 
-		this.plugin.registerDomEvent(monthEl, "click", () => {
-			new Notice("Month clicked!");
-			const COMMAND_ID = "periodic-notes:open-monthly-note";
-			const commands = (this.plugin.app as any).commands;
-			const exists = commands
-				.listCommands()
-				.some((cmd: any) => cmd.id === COMMAND_ID);
-
-			if (!exists) {
-				new Notice(`Command with ID "${COMMAND_ID}" does not exist.`);
-				return;
-			}
-
-			commands.executeCommandById(COMMAND_ID);
-		});
+		this.plugin.registerDomEvent(
+			monthEl,
+			"click",
+			this.clickMonthHandler.bind(this)
+		);
 	}
 }
